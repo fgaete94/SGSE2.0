f9f053265bf5a51c6dffb6bab07710c03f2050f1
fgaete94 <f.gaete@duocuc.cl>
2024-10-15 20:13:35 -0300

    service auth

--- SGSE2/src/environments/environment.ts
+++ SGSE2/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   api_url: "https://nfpulkphukfwoqsmpmaj.supabase.co/rest/v1",
-  apiKeySupabase: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mcHVsa3BodWtmd29xc21wbWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyMTk0MTgsImV4cCI6MjA0Mjc5NTQxOH0.Yc0or48cXI7dgP5y7ylxMRG2M_Sl3lyQJaKYdg0GOis" 
+  apiKeySupabase: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mcHVsa3BodWtmd29xc21wbWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyMTk0MTgsImV4cCI6MjA0Mjc5NTQxOH0.Yc0or48cXI7dgP5y7ylxMRG2M_Sl3lyQJaKYdg0GOis",
+  secretKey: "eW>9~NpjI6d~1((BO@rr7>arkMz):F8~ZNgI" 
 };
 
 /*
