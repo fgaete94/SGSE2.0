5f1cc4fd24830d96d1d131bd92e56f9ca80326da
Felipe Gaete Troncoso <f.gaete@duocuc.cl>
2024-10-24 12:41:22 -0300

    para ejecutar test

--- SGSE2/src/app/services/api-config/api-config.service.spec.ts
+++ SGSE2/src/app/services/api-config/api-config.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiConfigService } from './api-config.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('ApiConfigService', () => {
   let service: ApiConfigService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClient]});
     service = TestBed.inject(ApiConfigService);
   });
 
