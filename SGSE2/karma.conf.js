4db1cd9651caa35cccd362eaf361e818745bedd6
Felipe Gaete Troncoso <f.gaete@duocuc.cl>
2024-10-24 22:35:17 -0300

    isntale muchas cosas

--- SGSE2/karma.conf.js
+++ SGSE2/karma.conf.js
@@ -39,6 +39,9 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    files: [
+      { pattern: 'src/**/*.spec.ts', watched: false }
+    ]
   });
 };
